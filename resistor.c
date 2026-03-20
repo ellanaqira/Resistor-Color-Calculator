48da1c9518739a9672be7af3c9bd8c0e0005f678
ellanaqira <ellanrepo@gmail.com>
2026-03-21 02:14:28 +0800

    Refactor output formatting and add support for 5-band resistors

--- resistor.c
+++ resistor.c
@@ -16,14 +16,14 @@ int main()
     int ln_length;
 
     // separator line
-    for (ln_length = 1; ln_length < 100; ln_length++)
+    for (ln_length = 1; ln_length < 70; ln_length++)
     {
         printf("%s", ln);
     }
     // choose type of band
     int num_band;
     printf("\n# Select the number of band\n");
-    printf("  [ 3 bands = 3\t\t  ]\n  [ 4 bands = 4\t\t  ]\n  [ 5 bands = 5\t\t  ]\n  [ 6 bands = 6\t\t  ]\n  [ exit    = Ctr + c\t  ]\n");
+    printf("  [ 3 bands = 3\t\t  ]\n  [ 4 bands = 4\t\t  ]\n  [ 5 bands = 5\t\t  ]\n  [ 6 bands = 6\t\t  ]\n  [ Exit    = Ctr + c\t  ]\n");
     printf("  > ");
     scanf("%d", &num_band);
     while (num_band != 0)
@@ -32,7 +32,7 @@ int main()
         {
             if (num_band == 3)
             {
-                // Open band1.txt file
+                // Open 3bands.txt file
                 printf("\n    .--._____.--.");
                 printf("\n--=(|| 3 Bands  |)=----------");
                 printf("\n    '--'     '--'\n");
@@ -108,8 +108,8 @@ int main()
                     long result_3b = combine_3b * power_3b;
                     // print out the 3 bands result
                     char percent[] = "%";
-                    printf("\n-Result : %li ohm ± 20%s\n", result_3b, percent);
-                    printf("___________________________\n\n");  
+                    printf("___________________________\n");
+                    printf("\n-Result = %li ohm ± 20%s\n\n", result_3b, percent);
                 }
 
                 // if value of multi_3b >= -3 and < 0 (negative)
@@ -126,14 +126,14 @@ int main()
                     float result_3b = combine_3b * fpowmin_3b;
                     //print out the 3 bands result
                     char percent[] = "%";
-                    printf("\n-Result : %g ohm ± 20%s\n", result_3b, percent);
-                    printf("___________________________\n\n"); 
+                    printf("___________________________\n");
+                    printf("\n-Result = %g ohm ± 20%s\n\n", result_3b, percent); 
                 } 
             }
 
             if (num_band == 4)
             {
-                // Open band1.txt file
+                // Open 4bands.txt file
                 printf("\n    .--._____.--.");
                 printf("\n--=(|| 4 Bands ||)=----------");
                 printf("\n    '--'     '--'\n");
@@ -222,8 +222,8 @@ int main()
                     long result_3b = combine_3b * power_3b;
                     // print out the 4 bands result
                     char percent[] = "%";
-                    printf("\n-Result : %li ohm ± %g%s\n", result_3b, tolerance_4b[tolnum_4b], percent);
-                    printf("___________________________\n\n");  
+                    printf("___________________________\n");
+                    printf("\n-Result = %li ohm ± %g%s\n\n", result_3b, tolerance_4b[tolnum_4b], percent);
                 }
 
                 // if value of multi_4b >= -3 and < 0 (negative)
@@ -240,20 +240,152 @@ int main()
                     float result_3b = combine_3b * fpowmin_3b;
                     //print out the 4 bands result
                     char percent[] = "%";
-                    printf("\n-Result : %g ohm ± %g%s\n", result_3b, tolerance_4b[tolnum_4b], percent);
-                    printf("___________________________\n\n"); 
+                    printf("___________________________\n");
+                    printf("\n-Result = %g ohm ± %g%s\n\n", result_3b, tolerance_4b[tolnum_4b], percent);
                 } 
+            }
+
+            if (num_band == 5)
+            {
+                // Open 5bands.txt file
+                printf("\n    .--._____.--.");
+                printf("\n--=(|||5 Bands ||)=----------");
+                printf("\n    '--'     '--'\n");
+
+                char line_5b[2000];
+                FILE *hand_5b;
+                hand_5b = fopen("5bands.txt", "r");
+                while (fgets(line_5b, 2000, hand_5b) != NULL)
+                {
+                    printf("%s", line_5b);
+                }
+
+                // input warn
+                printf("\n _\n(i) Input must be a number that include in the tabel!\n");
+
+                // 1st 5 Bands Input menu
+                int band5_1;
+                printf("\n# Enter 1st Band color > ");
+                scanf("%d", &band5_1);
+                while (band5_1 < 1 || band5_1 > 9)
+                {
+                    printf("    _\n");
+                    printf("   /|\\  (%d) Not included!\n", band5_1);
+                    printf("  /_*_\\ 1st Band > ");
+                    scanf("%d", &band5_1);
+                }
+
+                // 2st 5 Bands Input menu
+                int band5_2;
+                printf("\n# Enter 2nd Band color > ");
+                scanf("%d", &band5_2);
+                while ( band5_2 < 0 || band5_2 > 9)
+                {
+                    printf("    _\n");
+                    printf("   /|\\  (%d) Not included!\n", band5_2);
+                    printf("  /_*_\\ 2nd Band > ");
+                    scanf("%d", &band5_2);
+                }
+
+                // 3rd 5 bands input menu
+                int band5_3;
+                printf("\n# Enter 3rd Band color > ");
+                scanf("%d", &band5_3);
+                while (band5_3 < 0 || band5_3 > 9)
+                {
+                    printf("    _\n");
+                    printf("   /|\\  (%d) Not included!\n", band5_3);
+                    printf("  /_*_\\ 3rd Band > ");
+                    scanf("%d", &band5_3);
+                }
+
+                // 4th multiplier 5 bands input menu
+                int multi_5b;
+                printf("\n# Enter 4rd Band color > ");
+                scanf("%d", &multi_5b);
+                while (multi_5b < -3 || multi_5b > 9)
+                {
+                    printf("    _\n");
+                    printf("   /|\\  (%d) Not included!\n", multi_5b);
+                    printf("  /_*_\\ 4th Band > ");
+                    scanf("%d", &multi_5b);
+                }
+
+                // 5th Tolerance 5 bands input menu
+                float tolerance_5b[] = {0, 1, 2, 0.05, 0.02, 0.5, 0.25, 0.1, 0.01, 5, 10};
+                int tolnum_5b;
+                printf("\n# Enter 5th Band color > ");
+                scanf("%d", &tolnum_5b);
+                while (tolnum_5b < 1 || tolnum_5b > 10)
+                {
+                    printf("    _\n");
+                    printf("   /|\\  (%d) Not included!\n", tolnum_5b);
+                    printf("  /_*_\\ 5th Band > ");
+                    scanf("%d", &tolnum_5b);
+                }
+
+                // Convert into str
+                char str_band5_1[10];
+                char str_band5_2[10];
+                char str_band5_3[10];
+                sprintf(str_band5_1, "%d", band5_1);
+                sprintf(str_band5_2, "%d", band5_2);
+                sprintf(str_band5_3, "%d", band5_3);
+
+                // combine str_band5_3 and str_band5_2 into str_band5_1
+                strcat(str_band5_2, str_band5_3);
+                strcat(str_band5_1, str_band5_2);
+                // convert into int
+                int combine_5b;
+                sscanf(str_band5_1 ,"%d", &combine_5b);
+
+                // make 10^multi_4b
+                // if value of multi_4b >= 0 and <= 9 (positive)
+                int i;
+                if (multi_5b >= 0 && multi_5b <= 9 )
+                {
+                    long power_5b = 1;
+                    for (i = 1; i <= multi_5b; i++)
+                    {
+                        power_5b = power_5b * 10;
+                    }
+                    // multiply the combine_5b with power_5b
+                    long result_5b = combine_5b * power_5b;
+                    // print out the 4 bands result
+                    char percent[] = "%";
+                    printf("___________________________\n");
+                    printf("\n-Result = %li ohm ± %g%s\n\n", result_5b, tolerance_5b[tolnum_5b], percent);  
+                }
+
+                // if value of multi_5b >= -3 and < 0 (negative)
+                else if (multi_5b >= -3 && multi_5b < 0)
+                {
+                    float fpower_5b = 1.0;
+                    for (i = 0; multi_5b < i; multi_5b++)
+                    {
+                        fpower_5b = fpower_5b * 10;
+                    }
+                    // divided 1 with fpower_5b
+                    float fpowmin_5b = 1 / fpower_5b;
+                    // multiply the combine_5b with fpowmin_5b
+                    float result_3b = combine_5b * fpowmin_5b;
+                    //print out the 4 bands result
+                    char percent[] = "%";
+                    printf("___________________________\n");
+                    printf("\n-Result = %g ohm ± %g%s\n\n", result_3b, tolerance_5b[tolnum_5b], percent); 
+                }
+
             }
 
             // separator line
             int ln_length;
-            for (ln_length = 1; ln_length < 100; ln_length++)
+            for (ln_length = 1; ln_length < 70; ln_length++)
             {
                 printf("%s", ln);
             }
             // choose type of band
             printf("\n# Select the number of band\n");
-            printf("  [ 3 bands = 3\t\t  ]\n  [ 4 bands = 4\t\t  ]\n  [ 5 bands = 5\t\t  ]\n  [ 6 bands = 6\t\t  ]\n  [ exit    = 0\t\t  ]\n");
+            printf("  [ 3 bands = 3\t\t  ]\n  [ 4 bands = 4\t\t  ]\n  [ 5 bands = 5\t\t  ]\n  [ 6 bands = 6\t\t  ]\n  [ Exit    = 0\t\t  ]\n");
             printf("  > ");
             scanf("%d", &num_band);
 
@@ -264,13 +396,13 @@ int main()
         {
 	        printf("  (!) Not Include!\n\n");
             // separator line
-            for (ln_length = 1; ln_length < 100; ln_length++)
+            for (ln_length = 1; ln_length < 70; ln_length++)
             {
                 printf("%s", ln);
             }
             // choose type of band
             printf("\n# Select the number of band\n");
-            printf("  [ 3 bands = 3\t\t  ]\n  [ 4 bands = 4\t\t  ]\n  [ 5 bands = 5\t\t  ]\n  [ 6 bands = 6\t\t  ]\n  [ exit    = 0\t\t  ]\n");
+            printf("  [ 3 bands = 3\t\t  ]\n  [ 4 bands = 4\t\t  ]\n  [ 5 bands = 5\t\t  ]\n  [ 6 bands = 6\t\t  ]\n  [ Exit    = 0\t\t  ]\n");
             printf("  > ");
             scanf("%d", &num_band);
         }
